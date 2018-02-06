4a49410253c98539e4bf7906cfe8476bc12de555
Sarah Ford <sarah.jeong.ford@gmail.com>
2018-02-05 18:22:42 -0800

    fixed help popup positioning

--- src/components/3_userInfo/unsuccessful.js
+++ src/components/3_userInfo/unsuccessful.js
@@ -8,7 +8,8 @@ const Unsuccessful = function(props) {
   return (
     <section className='unsuccessful'>
       <div>
-        <h4>Looks like there was an error processing your request. Please reach out to our customer service team for assistance or try submitting a
+        <h4>Looks like there was an error processing your request.</h4>
+        <h4>Please reach out to our customer service team for assistance or try submitting a
           <span onClick={props.startOver}> new request</span>.
         </h4>
         <Contact />
