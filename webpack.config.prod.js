4b975c242d918e373494ed6897e679db4b4b337e
Sarah Ford <sarah.jeong.ford@gmail.com>
2018-02-08 17:28:38 -0800

    added prod webpack file

---
+++ webpack.config.prod.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [ './src', ],
+
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/i,
+        loader: 'babel-loader',
+        options: {
+          presets: ['env'],
+          plugins: [
+            ['transform-react-jsx', { pragma: 'h' }]
+          ]
+        }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loader: 'style-loader!css-loader!sass-loader'
+      },
+      {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [{
+              loader: 'file-loader',
+              options: {
+                  name: '[name].[ext]',
+                  outputPath: 'fonts/'
+              }
+          }]
+      },
+    ],
+  }
+
+  // devtool: 'source-map',
+
+  // devServer: {
+  //   contentBase: path.join(__dirname, 'src'),
+  //   hot: true,
+  //   compress: true,
+  //   historyApiFallback: true
+  // }
+}
